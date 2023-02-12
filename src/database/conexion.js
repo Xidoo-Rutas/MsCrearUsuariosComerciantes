1535e6badb20d3eb4389288ad82f833f4247ffb0
Asur <78565438+dxASURxd@users.noreply.github.com>
2023-02-12 13:44:28 -0600

    cluster

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -5,7 +5,8 @@ require('dotenv').config()
 const databaseConnected = () =>{
     mongoose
     .set("strictQuery", true)
-    .connect(process.env.MONGODB_URI)   
+    //.connect(process.env.MONGODB_URI)
+    .connect('mongodb://127.0.0.1:27017/xidoo');
     .then(() => console.log("Se ha enlazado a la base de datos del servidor"))
     .catch((error) =>console.error("'Ocurrio un error al conectarse a la base de datos del servidor"))
 }
